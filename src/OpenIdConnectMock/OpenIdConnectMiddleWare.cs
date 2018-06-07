b518254d18ebf21fbe361c955cd6bc1200d1fb3f
Pooventhan Supramaniam <pooventhan.supramaniam@my.experian.com>
2018-06-07 15:44:28 +0800

    Middleware implementation.

---
+++ src/OpenIdConnectMock/OpenIdConnectMiddleWare.cs
@@ -0,0 +1,54 @@
+﻿namespace OpenIdConnectMock
+{
+    using System;
+    using System.Threading.Tasks;
+    using Microsoft.Owin;
+
+    public class OpenIdConnectMiddleWare : OwinMiddleware
+    {
+        private readonly Uri baseUri;
+
+        public OpenIdConnectMiddleWare(Uri baseUri, OwinMiddleware next)
+            : base(next)
+        {
+            this.baseUri = baseUri;
+        }
+
+        public override async Task Invoke(IOwinContext context)
+        {
+            if (context.Request.Path.Value.Contains("openid-configuration"))
+            {
+                await HandleOpenIdConfigurationRequest(context);
+            }
+
+            if (context.Request.Path.Value.Contains("keys"))
+            {
+                await HandleKeysRequest(context);
+            }
+
+            if (context.Request.Path.Value.Contains("authorize"))
+            {
+                await HandleKeysRequest(context);
+            }
+
+
+            if (context.Request.Path.Value.Contains("token"))
+            {
+                await HandleKeysRequest(context);
+            }
+
+
+            await Next.Invoke(context);
+        }
+
+        private async Task HandleOpenIdConfigurationRequest(IOwinContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        private async Task HandleKeysRequest(IOwinContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
